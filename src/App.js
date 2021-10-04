70351f7a62e9f5ccd901fa9744ea00cec21c7de3
whynotme1131999 <whynotme1131999@gmail.com>
2021-10-04 21:47:34 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { Bar } from 'react-chartjs-2';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Bar
+        data={{
+          labels: ['blue', 'red', 'w', 'ds', 'sds'],
+          datasets: [
+            {
+              label: "#vote",
+              data: [1, 2, 3, 4, 5]
+            }
+          ]
+
+        }}
+        width={600}
+        height={300}
+        options={{
+          maintainAspectRatio: false
+        }}
+      />
     </div>
   );
 }
